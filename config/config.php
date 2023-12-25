0972da6af34f1ad79a31b0ab5c13863f6c4ef6be
abdeljabaraitayoub <abdeljabarayoubi@gmail.com>
2023-12-25 11:14:00 +0100

    skeleton

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// config.php
-
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'jobeasy');
--- public/index.php
+++ public/index.php
@@ -1,39 +1,11 @@
 <?php
+require '../vendor/autoload.php';
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "./../");
+$dotenv->load();
 
-use App\Controllers\HomeController;
-use App\Controllers\LoginController;
+// print_r($_ENV);
+$router = require '../src/Routes/index.php';
 
 
 
-$route = isset($_GET['route']) ? $_GET['route'] : 'home';
-
-// Instantiate the controller based on the route
-switch ($route) {
-    case 'home':
-        $controller = new HomeController();
-        $controller->index();
-        break;
-    case 'fetchMoreUsers':
-        $controller = new HomeController();
-        $controller->fetchMoreUsers();
-        break;
-    case 'login':
-        $logincontroller = new LoginController();
-        $logincontroller->login();
-        break;
-    case 'creat':
-            $logincontroller = new LoginController();
-            $logincontroller->creat();
-            break;
-    // Add more cases for other routes as needed
-    default:
-        // Handle 404 or redirect to the default route
-        header('HTTP/1.0 404 Not Found');
-        exit('Page not found');
-}
-
-// Execute the controller action
-
-?>
